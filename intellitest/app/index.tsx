8786fc7c03374f54dab9d7c8faf2b0bf2cb09fe1
austinatividad <austin_natividad@dlsu.edu.ph>
2024-10-17 09:45:28 +0800

    feat: implemented splash screen

--- intellitest/app/index.tsx
+++ intellitest/app/index.tsx
@@ -1,17 +1,37 @@
 import { View } from "react-native";
 import { Text } from "@/components/ui/text";
 import { useLoadingContext } from "@/components/Providers/LoaderSpinnerContext";
+import { Button } from "@/components/ui/button";
+import { Image } from "lucide-react-native";
 export default function Index() {
   const { setLoading, setText } = useLoadingContext();
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
+        justifyContent: "flex-end",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-col gap-48 pb-8">
+      <View>
+        {/* TODO: Change to a carousel */}
+        <View className="flex flex-row justify-center">
+          <Image name="Test" size={300} />
+        </View> 
+        
+        <Text className="text-4xl font-bold">Ready to <Text className="text-green-400 text-4xl font-bold">ace</Text> your next test?</Text>
+        <Text className="text-2xl font-bold ">Let's get started!</Text>
+      </View>
+      
+
+      <View className="">
+        <Button>
+          <Text className="text-2xl">Continue</Text>
+        </Button>
+      </View>
+    </View>
+      
     </View>
   );
 }

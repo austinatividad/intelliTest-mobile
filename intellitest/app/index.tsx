4e038adf0200347149c9bbf9db0ee93a096ee2b7
Luis Miguel Antonio Razon <luismiguelrazon1121@gmail.com>
2024-12-08 02:31:18 +0800

    fix: do not load page when session not confirmed yet

--- intellitest/app/index.tsx
+++ intellitest/app/index.tsx
@@ -11,49 +11,62 @@ import { useLoadingContext } from "@/components/Providers/LoaderSpinnerContext";
 export default function Index() {
   const router = useRouter();
   const { setLoading, setText } = useLoadingContext();
-
+  const [sessionChecked, setSessionChecked] = React.useState(false);
 
   React.useEffect(() => {
     // setLoading(true);
     // setText(`Welcome! Please be patient while we prepare things for you! 😊`);
     async function checkSession() {
+      setLoading(true);
+      setText(``);
       const session = await getSession();
-      setLoading(false)
       if (session.data.session != null) {
         router.replace("/dashboard");
       }
+
+      setSessionChecked(true);
     }
     checkSession();
     
   }, [router]);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "flex-end",
-        alignItems: "center",
-      }}
-    >
-      <View className="flex-col gap-48 pb-8 p-2">
-        <View>
-          <View className="flex flex-row justify-center">
-            <Image
-              source={require("../assets/images/splash_1.png")}
-              style={{ width: 300, height: 300 }}
-            />
+    <View>
+
+    {sessionChecked && 
+      <>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "flex-end",
+            alignItems: "center",
+          }}
+        >
+          <View className="flex-col gap-48 pb-8 p-2">
+            <View>
+              <View className="flex flex-row justify-center">
+                <Image
+                  source={require("../assets/images/splash_1.png")}
+                  style={{ width: 300, height: 300 }}
+                />
+              </View>
+              <Text className="text-4xl font-bold">
+                Ready to <Text className="text-green-400 text-4xl font-bold">ace</Text> your next big test?
+              </Text>
+              <Text className="text-2xl font-bold">Let's get started!</Text>
+            </View>
+            <View>
+              <Button onPress={() => router.push("./auth")}>
+                <Text className="text-2xl">Continue</Text>
+              </Button>
+            </View>
           </View>
-          <Text className="text-4xl font-bold">
-            Ready to <Text className="text-green-400 text-4xl font-bold">ace</Text> your next big test?
-          </Text>
-          <Text className="text-2xl font-bold">Let's get started!</Text>
         </View>
-        <View>
-          <Button onPress={() => router.push("./auth")}>
-            <Text className="text-2xl">Continue</Text>
-          </Button>
-        </View>
-      </View>
+      </>
+    }
+
     </View>
+
+
   );
 }
